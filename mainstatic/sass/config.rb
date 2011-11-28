6b5a2710256d9287016cc2a4a70463a7a4a0d88c
Benjamin Sternthal <ben@devpatch.com>
2011-11-28 10:07:42 -0800

    stubbing out sass and includes

---
+++ mainstatic/sass/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "../stylesheets"
+sass_dir = "."
+images_dir = "../img/layout"
+javascripts_dir = "../js"
+http_images_path = "/static/img/layout"
+
+
+#uncomment for production
+line_comments = false
+output_style = :compressed

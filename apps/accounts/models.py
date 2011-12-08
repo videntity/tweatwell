edff72fb4ea2db8133e6e57c94fcd97b67c67524
Alan Viars <aviars@videntity.com>
2011-12-08 13:31:22 -0500

    removed twilio req.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -7,36 +7,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date, datetime, timedelta
 from django.contrib.localflavor.us.models import PhoneNumberField
-from sms_utils import send_sms_twilio
 import string
 import random
 import uuid
 from emails import send_password_reset_url_via_email, send_signup_key_via_email
 from django.utils.safestring import mark_safe
 
-class ValidSMSCode(models.Model):
-    user               = models.ForeignKey(User)
-    sms_code           = models.CharField(max_length=4, blank=True)
-    expires            = models.DateTimeField(default=datetime.now)
-                           
-
-    def __unicode__(self):
-        return '%s for user %s expires at %s' % (self.sms_code,
-                                                 self.user.username,
-                                                 self.expires)
-        
-    def save(self, **kwargs):
-        up=self.user.get_profile()
-        randcode=random.randint(1000,9999)
-        if not self.sms_code:
-            self.sms_code=randcode
-        now = datetime.now()
-        expires=now+timedelta(minutes=settings.SMS_LOGIN_TIMEOUT_MIN)
-        self.expires=expires
-        new_number="+1%s" %(string.replace(str(up.mobile_phone_number),"-", ""))
-        #send an sms code
-        x=send_sms_twilio(twilio_body=self.sms_code, twilio_to=new_number)
-        super(ValidSMSCode, self).save(**kwargs)
 
 
 class ValidPasswordResetKey(models.Model):

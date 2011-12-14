6a5baa241215e699cf5ac026d36d8b03ee2dbd9c
Alan Viars <aviars@videntity.com>
2011-12-14 13:57:27 -0500

    Fixed the email bug in account settings

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -120,7 +120,7 @@ class AccountSettingsForm(forms.Form):
     def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).count() > 0:
+        if email and User.objects.filter(email=email).exclude(email=email).count():
             raise forms.ValidationError(u'This email address is already registered.')
         return email
 

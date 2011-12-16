5be7e26268a0070a6b63767716f5a06769630aeb
Benjamin Sternthal <ben@devpatch.com>
2011-12-16 13:51:20 -0800

    hacked the show hide of other veg entry

--- mainstatic/js/lib/page/checkin.js
+++ mainstatic/js/lib/page/checkin.js
@@ -9,9 +9,24 @@
  */
 
 $(function() {
-    //comments
+    //comments show hide
     $('.tw-comment-trigger').click(function(){
         $(this).parent('.tweat-comments').find('.tweat-comment-form').slideToggle();
         return false;
     });
+
+    //other text field show hide
+    $('#id_freggie').change(function() {
+        var option = $('#id_freggie' + ' option:selected').val();
+        var label = "label[for=id_freggie_other]";
+        var fieldId = "#id_freggie_other";
+
+        if ( (option == "other_fruit") || (option == "other_veg") ) {
+            $(fieldId + ', ' + label).fadeIn('fast');
+            $(label).css('display', 'block');
+        } else {
+            $(fieldId + ', ' + label).fadeOut('fast');
+        }
+    });
+
 });

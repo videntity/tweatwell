7c7d319ed5213b1b60602dc8f9224c4145043f07
Alan Viars <aviars@videntity.com>
2011-12-18 13:50:13 -0500

    Added tip of the day

--- apps/checkin/views.py
+++ apps/checkin/views.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 from django.conf import settings
+from django.db.models import Sum
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 from ..accounts.models import UserProfile
 from ..questions.models import QuestionAnswer, Question
-from forms import FreggieForm, CommentForm, NonVegForm
-from ..upload.forms import PickFruitForm, PickVeggieForm
-import datetime, os, pycurl, StringIO, json, types, sys
-from operator import itemgetter, attrgetter
-from django.contrib import messages
-from models import Comment, Freggie, NonVeg
 from ..roulette.models import Roulette
-from django.db.models import Sum
+from ..tips.models import CurrentTip
+from ..upload.forms import PickFruitForm, PickVeggieForm
+import datetime
+from forms import FreggieForm, CommentForm, NonVegForm
+from models import Comment, Freggie, NonVeg
 from itertools import chain
 from operator import attrgetter
 
@@ -85,25 +85,36 @@ def nonveg(request):
             return HttpResponseRedirect(reverse('checkin'))
 
 
+            
 @login_required
-def freggie_comment(request):
+def freggie_comment(request, freggie_id):
+    
+    freggie = get_object_or_404(Freggie, pk=freggie_id)
     
     if request.method == 'POST':
 
-        form = NonVegForm(request.POST)
+        form = CommentForm(request.POST)
         
         if form.is_valid():  
             data = form.cleaned_data
-            newnonveg=form.save(commit=False)
-            newnonveg.text=data['text']
-            newnonveg.nonveg=data['nonveg']
-            newnonveg.user=request.user
+            newcomment=form.save(commit=False)
+            newcomment.text=data['text']
+            newcomment.user=request.user
+            newcomment.freggie=freggie
             newcomment.save()
-            messages.success(request, "Successfuly added a non fruit or vegetable item.")
+            messages.success(request, "Successfuly added a comment.")
             return HttpResponseRedirect(reverse('checkin'))
             
 def checkin(request):
 
+
+    #get the current tip
+    try:
+        ct=CurrentTip.objects.all()[0]
+        tiptext=ct.tip.text
+    except(CurrentTip.DoesNotExist):
+        tip_text="No tip to display"
+    
     #if the user is not logged in, display an anonymous home page
     if request.user.is_anonymous()==True:
         return anon_home_index(request)
@@ -166,6 +177,7 @@ def checkin(request):
                  'question':question,
                  'tweatlist': tweatlist,
                  'freggies': freggies,
+                 'tip_text':tip_text,
                  'freggie_points': freggie_points,
                  'comment_points': comment_points,
                  'roulette_points': roulette_points,         
@@ -179,6 +191,7 @@ def checkin(request):
                  'question':question,
                 'tweatlist': tweatlist,
                 'freggies': freggies,
+                'tip_text':tip_text,
                 'freggie_points': freggie_points,
                 'comment_points': comment_points,
                 'roulette_points': roulette_points,         
--- apps/tips/models.py
+++ apps/tips/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Tip(models.Model):
+    text         = models.TextField(max_length=1000)
+    date         = models.DateField(auto_now_add=True)
+    class Meta:
+        ordering = ['-date']
+    
+    def __unicode__(self):
+        return '%s ' % (self.text)
+
+
+
+class CurrentTip(models.Model):
+    tip          = models.ForeignKey(Tip)
+    date         = models.DateField(auto_now_add=True)
+    class Meta:
+        ordering = ['-date']
+    
+    def __unicode__(self):
+        return '%s ' % (self.tip)

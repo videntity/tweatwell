e01e081b481746ff11a864bf8a4fe072e69c5f0c
Benjamin Sternthal <ben@devpatch.com>
2011-12-29 10:01:20 -0800

    added sample_settings removed settings_local, local setting should not be in git

---
+++ config/sample_settings_local.py
@@ -0,0 +1,10 @@
+#local settings -------------------------------------------------------------
+STATIC_URL = 'http://tweatwellstatic.s3.amazonaws.com/static/'
+ADMIN_MEDIA_PREFIX = 'http://djadminstatic.s3.amazonaws.com/'
+
+HOSTNAME_URL = 'http://app.tweatwell.com'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+#Cron key required to run scoreing and twitbot urls.
+CRON_KEY=''

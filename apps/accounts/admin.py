afbb03bf59317377a588d9cd3283d90bdbe1e476
Alan Viars <aviars@videntity.com>
2012-01-10 21:09:51 -0500

    Enhanced userprofile admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -3,7 +3,13 @@ from models import UserProfile, Permission, ValidPasswordResetKey, \
                     ValidSignupKey
 
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','twitter', 'dean_fruit_badge',
+                    'dean_veggie_badge', 'joker_badge',
+                    'president_badge')
+    search_fields = ['user__email', 'twitter']
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Permission)
 admin.site.register(ValidPasswordResetKey)
 admin.site.register(ValidSignupKey)

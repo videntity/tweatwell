5caa8fd1bbcbd678a31bbc83397ccfc7c3ce7fe7
Alan Viars <aviars@videntity.com>
2012-03-08 23:30:07 -0500

    Fixed QOW configuration error

--- settings.py
+++ settings.py
@@ -154,7 +154,7 @@ INSTALLED_APPS = (
     'tweatwell.apps.recipes',
     'tweatwell.apps.tips',
     'tweatwell.apps.accounts',
-    'tweatwell.apps.quiz',
+    'tweatwell.apps.questions',
     'tweatwell.apps.profile',
     'tweatwell.apps.leaderboard',
     'avatar',
@@ -224,7 +224,7 @@ TWITTERHASH="%23freggie"
 
 
 #Cron key required to run scoreing and twitbot urls.
-CRON_KEY="c8751d86-b82e-474f-89a7-098ffdf26bdb"
+CRON_KEY="c8751d86-b82e-474f-89a7-098ffdf26bzz"
 
 # TIMEZONE OFFSET
 TIMEZONE_OFFSET=-5
